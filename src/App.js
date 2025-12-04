2c283a7f2260e13a6a17a183773f5393c30edd90
Ronny Wouters <ozzy.wouters@telenet.be>
2025-12-04 20:21:38 +0100

    goede form als basis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import {Person} from "./components/Person";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Person p={{name : 'Ronny' ,age : 57}}/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Person.js
@@ -0,0 +1,32 @@
+import {useState} from "react";
+
+export function Person({p}) {
+    const [person, setPerson] = useState(p)
+    const [old, setOld] = useState(p)
+
+    const [mode, setMode] = useState('R')
+
+    return <div className='form'>
+        <label htmlFor="name">Name</label>
+        <input id="name" className="input-field" type="text" disabled={mode === 'R'} value={person.name}
+               onChange={e => setPerson({...person, name: e.target.value})}/>
+
+        <label htmlFor="age">Age</label>
+        <input id="age" className="input-field" type="number" disabled={mode === 'R'} value={person.age}
+               onChange={e => setPerson({...person, age: e.target.value})}/>
+
+        <button className="form-button" disabled={mode === 'W'}
+                onClick={(e) => setMode('W')}>Update
+        </button>
+
+        <button className="form-button" disabled={mode === 'R'}
+                onClick={(e) =>  {setPerson(old); setMode('R')}}>Cancel
+        </button>
+
+        <button className="form-button" disabled={mode === 'R'}
+                onClick={(e) => {setMode('R'); setOld(person)} }>Save
+        </button>
+    </div>
+}
+
+
