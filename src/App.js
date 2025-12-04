c4fe14caafa69310fd196618affd75bf63ccf849
Ronny Wouters <ozzy.wouters@telenet.be>
2025-12-04 20:46:30 +0100

    Titels toegevoegdy

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
+                <h1>Effectenrekening</h1>
+                <h2>Titularis</h2>
                 <Person p={{name : 'Ronny' ,age : 57}}/>
             </header>
         </div>
